31095397ca85c6d3ae214e2417ff8dfdfc7cb86e
Jeff <15724935+jeffffc@users.noreply.github.com>
2026-02-23 12:12:10 +0800

    add db migrations

---
+++ Mud9Bot.Data/Migrations/20260222134120_add welcome.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mud9Bot.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addwelcome : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "welcomephoto",
+                table: "groups",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "welcometext",
+                table: "groups",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "welcomephoto",
+                table: "groups");
+
+            migrationBuilder.DropColumn(
+                name: "welcometext",
+                table: "groups");
+        }
+    }
+}
---
+++ Mud9Bot.Data/Migrations/20260223031523_add stats.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Mud9Bot.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addstats : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "bot_event_logs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    EventType = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Metadata = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    ChatType = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Count = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_bot_event_logs", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_bot_event_logs_EventType_Metadata_ChatType",
+                table: "bot_event_logs",
+                columns: new[] { "EventType", "Metadata", "ChatType" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "bot_event_logs");
+        }
+    }
+}
